4a8c6150317ff8a528b235613d103f90d5c18b90
Yassine <22yassine05@gmail.com>
2019-12-28 17:00:04 +0100

    Merge branch 'master' of https://github.com/YassineGallaoui/PorgettoMobile
    # Conflicts:
    #	app/src/main/java/mobile/computing/project/ListElement.java
    #	app/src/main/java/mobile/computing/project/Profilo.java
    #	app/src/main/java/mobile/computing/project/fClassifica.java

--- app/src/main/java/mobile/computing/project/SplashScreen.java
+++ app/src/main/java/mobile/computing/project/SplashScreen.java
@@ -1,31 +1,41 @@
 package mobile.computing.project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class SplashScreen extends Activity {
 
     private ImageView miaImmagine;
-
+    int t=1000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         miaImmagine = (ImageView) findViewById(R.id.splashImageView);
         miaImmagine.setImageResource(R.mipmap.splash_image);
+
         Thread timerThread = new Thread() {
             public void run() {
                 try {
-                    sleep(1000);
+                    sleep(t);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
-                    Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-                    startActivity(intent);
+                    //CONTROLLO SE INTERNET È ACCESO
+                    ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                    while(true){
+                        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                            //SIAMO CONNESSI A UNA QUALCHE RETE
+                            Intent vaiAlGioco=new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(vaiAlGioco);
+                        }
+                    }
                 }
             }
         };
